6983226c28a5d862ab61fb152c3f71f8fc201867
Wilson Mesquita <wylllson@yahoo.com.br>
2018-10-11 20:34:27 -0300

    Criação do DropBox Clone

---
+++ app/public/src/controller/DropBoxController.js
@@ -0,0 +1,64 @@
+class DropBoxController {
+
+    constructor(){
+        this.btnSendFileEl = document.querySelector('#btn-send-file');
+        this.inputFilesEl = document.querySelector('#files');
+        this.snackModalEl = document.querySelector('#react-snackbar-root');
+        this.initialEvents();
+    }
+
+    initialEvents(){
+
+        this.btnSendFileEl.addEventListener('click', event => {
+
+            this.inputFilesEl.click();
+
+        });
+
+        this.inputFilesEl.addEventListener('change', event => {
+
+            this.uploadTask(event.target.files);
+
+            this.snackModalEl.style.display = 'block';
+        });
+    }
+
+    uploadTask(files){
+
+        let promises = [];
+
+        [...files].forEach(file => {
+
+            promises.push(new Promise((resolve, reject) => {
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event =>{
+
+                    try {
+                        resolve(JSON.parse(ajax.eponseText));
+                    } catch (e) {
+                        reject(e);
+                    }
+
+                };
+
+                ajax.onerror = event => {
+                    reject(event);
+                };
+
+                let formData = new FormData();
+
+                formData.append('input-file', file);
+
+                ajax.send(formData);
+
+            }));
+
+        });
+
+        return promises.all(promises);
+    }
+}
